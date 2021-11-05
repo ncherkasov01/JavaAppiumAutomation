1e56edfee2d882b027d6fc6643ffbecf80df0ec9
Nikolai Cherkasov <ncherkasov.01@gmail.com>
2021-11-06 01:13:59 +0300

    Solution lesson 1 test 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,10 @@
 public class MainClass {
+    private String class_string = "Hello, world";
+    public String getClassString()
+    {
+        return class_string;
+    }
+
 
     public int getLocalNumber()
     {
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,4 +11,25 @@ public class MainClassTest extends MainClass
 
         Assert.assertTrue("wrong local number", localNumber == number);
     }
+    @Test
+    public void testGetClassString()
+    {
+        String expected = getClassString();
+        String actual = "Hello";
+        String actual2 = "hello";
+
+        if (expected.contains(actual))
+        {
+            System.out.println("Hello is present");
+        }
+        else  if (expected.contains(actual2))
+        {
+            System.out.println("hello is present");
+        }
+        else
+        {
+            Assert.fail("There is no Hello or hello");
+        }
+    }
+
 }
